ac71a371199d616d9a8bf18605e0faa8ce39c5bb
soicchi <soicchi@icloud.com>
2024-11-11 01:29:09 +0900

    internal: added test code to mask function for logger

--- app/internal/presentation/middlewares/logger_test.go
+++ app/internal/presentation/middlewares/logger_test.go
@@ -1,10 +1,67 @@
 package middlewares
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/soicchi/book_order_system/internal/logging"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+func TestCustomBodyDump(t *testing.T) {
+	tests := []struct {
+		name     string
+		body     string
+		mockFunc func(*testing.T, *logging.MockLogger)
+	}{
+		{
+			name: "test custom body dump",
+			body: `{
+				"name": "test",
+				"email": "test@test.co.jp",
+				"password": "password"
+			}`,
+			mockFunc: func(t *testing.T, m *logging.MockLogger) {
+				m.On("LogAttrs", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			logger := logging.NewMockLogger()
+			tt.mockFunc(t, logger)
+
+			e := echo.New()
+
+			// set middleware
+			e.Use(middleware.BodyDump(CustomBodyDump(logger)))
+
+			// request handler for test
+			e.POST("/test", func(c echo.Context) error {
+				return c.JSON(200, map[string]interface{}{"message": "ok"})
+			})
+
+			// set up test request
+			req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(tt.body))
+			rec := httptest.NewRecorder()
+
+			e.ServeHTTP(rec, req)
+
+			logger.AssertExpectations(t)
+		})
+	}
+}
 
 func TestShouldMaskField(t *testing.T) {
 	tests := []struct {
