e062b544b438aadabeb6af2a6b9e061c768b5473
soicchi <soicchi@icloud.com>
2024-11-24 18:19:52 +0900

    feature: refresh

--- app/internal/domain/customer/repository.go
+++
@@ -1,9 +0,0 @@
-package customer
-
-import (
-	"github.com/labstack/echo/v4"
-)
-
-type Repository interface {
-	Create(ctx echo.Context, customer *Customer) error
-}
--- app/internal/presentation/handlers/customers/request.go
+++
@@ -1,6 +0,0 @@
-package customers
-
-type CreateCustomerRequest struct {
-	Name  string `json:"name" validate:"required"`
-	Email string `json:"email" validate:"required,email"`
-}
